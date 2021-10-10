da5726ab692f1e8cf8cad99d5c3f35e4f12f606a
giselleariadna <giselleariadna@gmail.com>
2021-10-10 19:06:53 -0300

    cositas nuevas

---
+++ DIM-WEB/DIM-WEB/Scripts/dim-admin.js
@@ -0,0 +1,24 @@
+﻿////////////////////////////   ADMIN  START  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+$(document).ready(function () {
+    debugger;
+
+    let user = $("#user");
+    let password = $("#password");
+   
+    $("#submit").click(function () {
+        if (user == 'admin' && password == '1234') {
+            $("#login").hide();
+        } else {
+            $("#alerta-user-incorrecto").show();
+        }
+      
+
+    });
+
+
+
+});
+
+*/
